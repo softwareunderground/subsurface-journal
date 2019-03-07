f804c6ae83b358b1631b3a5e0d3e31407e1a237b
Filippo B <filippo82@users.noreply.github.com>
2019-03-07 08:03:14 +0100

    Modify conf.py.

--- docs/source/conf_original.py
+++ docs/source/conf_original.py
@@ -50,7 +50,7 @@ autodoc_default_flags = ['members', 'special-members', 'show-inheritance']
 autosummary_generate = True
 
 # Add any paths that contain templates here, relative to this directory.
-#templates_path = ['_templates']
+templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
